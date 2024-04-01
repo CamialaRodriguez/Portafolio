b91f2a1b35df3ea92bc0fc75bdcf141ca94a1f39
Camila Rodriguez <137795930+CamialaRodriguez@users.noreply.github.com>
2024-04-01 17:15:20 -0500

    Commit Initial

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+  
+
+// var lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+// window.addEventListener('scroll', function() {
+//     var image = document.querySelector('.columna-2 img');
+//     var windowHeight = window.innerHeight;
+//     var imagePosition = image.getBoundingClientRect().top;
+//     var currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//     var scrollDirection;
+
+//     if (currentScrollTop > lastScrollTop) {
+   
+//         scrollDirection = 'down';
+//     } else {
+
+//       scrollDirection = 'up';
+//     }
+
+//     lastScrollTop = currentScrollTop;
+
+//     if ((imagePosition < windowHeight / 2 && scrollDirection === 'down') || (imagePosition >= 0 && scrollDirection === 'up')) {
+//         image.classList.add('show');
+//     } else {
+
+//         image.classList.remove('show');
+//     }
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     setTimeout(function() {
+//         document.querySelector('.izq').style.animation = "fadein 2s forwards";  
+//         document.querySelector('.der').style.animation = "fadein 2s forwards";  
+//     }, 2000);
+
+//     window.addEventListener('scroll', function() {
+//         if (window.scrollY > 0) {
+//             document.querySelector('.izq').style.animation = "fadeout 2s forwards"; 
+//             document.querySelector('.der').style.animation = "fadeout 2s forwards"; 
+//         } else {
+//             document.querySelector('.izq').style.animation = "slidein-left 2s forwards, fadein 2s forwards"; 
+//             document.querySelector('.der').style.animation = "slidein-right 2s forwards, fadein 2s forwards"; 
+//         }
+//     });
+// });
+
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     setTimeout(function() {
+//         document.querySelector('.foto').style.animation = "fadein 2s forwards";
+//     }, 2000); 
+
+    
+// });
+
+
+
+
